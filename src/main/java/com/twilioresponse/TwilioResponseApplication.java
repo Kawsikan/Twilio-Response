f575bba5aed1323b70e47190e8a9eb53e0d44f13
kawsikan <kawsinote@gmail.com>
2021-09-14 18:51:37 +0530

    added gather methost

--- src/main/java/com/twilioresponse/TwilioResponseApplication.java
+++ src/main/java/com/twilioresponse/TwilioResponseApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwilioResponseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwilioResponseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TwilioResponseApplication.class, args);
+    }
 
 }
--- src/main/java/com/twilioresponse/twimlresponse/TwimlResponseController.java
+++ src/main/java/com/twilioresponse/twimlresponse/TwimlResponseController.java
@@ -1,20 +1,33 @@
 package com.twilioresponse.twimlresponse;
 
+import com.twilio.twiml.TwiML;
+import com.twilio.twiml.TwiMLException;
 import com.twilio.twiml.VoiceResponse;
+import com.twilio.twiml.voice.Gather;
 import com.twilio.twiml.voice.Play;
+import com.twilio.twiml.voice.Redirect;
 import com.twilio.twiml.voice.Say;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class TwimlResponseController {
 
+    @GetMapping(path = "/")
+    public String welcome() {
+        return "Hello World";
+    }
+
     private AtomicInteger callerNumber = new AtomicInteger();
 
-    @GetMapping(path = "/call", produces = "application/xml")
+    @PostMapping(path = "/call", produces = "application/xml")
     @ResponseBody
     public String respondToPhoneCall() {
 
@@ -25,8 +38,34 @@ public class TwimlResponseController {
 
     }
 
-    @GetMapping(path = "/")
-    public String welcome() {
-        return "Hello World";
+    @PostMapping("/gather")
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        TwiML twiml = new VoiceResponse.Builder()
+                .gather(
+                        new Gather.Builder()
+                                .numDigits(1)
+                                .say(new Say.Builder("For sales, press 1. For support, press 2.").build())
+                                .build()
+                )
+                .redirect(new Redirect.Builder("/voice").build())
+                .build();
+
+        response.setContentType("application/xml");
+        try {
+            response.getWriter().print(twiml.toXml());
+        } catch (TwiMLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private static void appendGather(VoiceResponse.Builder builder) {
+        builder.gather(new Gather.Builder()
+                .numDigits(1)
+                .say(new Say.Builder("For sales, press 1. For support, press 2.").build())
+                .build()
+        )
+                .redirect(new Redirect.Builder("/voice").build());
+    }
+
 }
